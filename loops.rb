ba3f5c08a8721aad5bd28ff562bef216f55caccf
Brian <riceb53@gmail.com>
2017-11-21 18:26:05 -0600

    Do loop practice

---
+++ loops.rb
@@ -0,0 +1,30 @@
+numbers = [1, 2, 4, 2]
+
+# times loop
+# i = 0
+# numbers.length.times do
+#   p numbers[i]
+#   # i = i + 1
+#   i += 1
+# end
+
+# while loop
+# i = 0
+# while true
+#   p numbers[i]
+#   i += 1
+#   if i > numbers.length
+#     break
+#   end
+# end
+
+# each loop
+
+# destructive
+# numbers = numbers.reverse
+# numbers.reverse!
+
+
+# numbers.each do |number|
+#   p number
+# end
